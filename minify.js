208efaeb4ae4bbfdda0ad570ed7c8a674bfa4da9
coderaiser <mnemonic.enemy@gmail.com>
2012-09-12 08:44:48 -0400

    moved all changed back

--- minify.js
+++ minify.js
@@ -10,7 +10,11 @@ console.log('minify.js loaded...');
  */
  
 var fs                  = require('fs'),
-    path                = require('path');
+    path                = require('path'),
+    crypto              = require('crypto');
+
+/* object contains hashes of files*/
+var Hashes;
 
 var html                = cloudRequire('./lib/html');
 var js                  = cloudRequire('./lib/js');
@@ -378,4 +382,52 @@ function cloudRequire(pModule){
   catch(pError){
       return false;
   }
+}
+
+function isFileChanged(pFileName, pFileData, pLastFile_b){
+        var lReadedHash;
+        
+        /* boolean hashes.json changed or not */
+        var lHashesChanged_b = false;
+        
+        if(!Hashes)
+            try {
+                console.log('trying  to read hashes.json');
+                Hashes = require(process.cwd() + '/hashes');
+                
+            }catch(pError) {
+                console.log('hashes.json not found... \n');
+                Hashes = {};
+            }
+        
+        for(var lFileName in Hashes)
+            /* if founded row with
+             * file name - save hash
+             */
+            if (lFileName === pFileName) {
+                lReadedHash = Hashes[pFileName];
+                break;
+        }
+        
+        /* create hash of file data */ 
+        var lFileHash = crypto.createHash('sha1');
+        lFileHash = crypto.createHash('sha1'); 
+        lFileHash.update(pFileData);
+        lFileHash = lFileHash.digest('hex');
+                
+        if(lReadedHash !== lFileHash){
+            Hashes[pFileName] = lFileHash;
+            lHashesChanged_b = true;
+        }
+                                
+        if(pLastFile_b){                                    
+            /* if hashes file was changes - write it */
+            if(lHashesChanged_b)
+                fs.writeFile('./hashes.json',
+                    JSON.stringify(Hashes),
+                    fileWrited('./hashes.json'));
+            else console.log('no one file has been changed');
+        }
+        /* has file changed? */
+        return lHashesChanged_b;
 }
