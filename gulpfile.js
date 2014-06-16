bfd27a836589327f2e6102a0fe2c3b60e902ac2c
coderaiser <mnemonic.enemy@gmail.com>
2014-06-16 02:32:24 -0400

    feature(gulp) add

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+    
+    var gulp        = require('gulp'),
+        jshint      = require('gulp-jshint'),
+        
+        Util        = require('util-io'),
+        fs          = require('fs'),
+        exec        = require('child_process').exec,
+        Info        = require('./package'),
+        
+        LIB         = './lib/',
+        Src         = [
+            './*.js',
+            'test/**/*.js',
+            LIB + '/**/*.js'
+        ];
+    
+    gulp.task('jshint', function() {
+        gulp.src(Src)
+            .pipe(jshint())
+            .pipe(jshint.reporter())
+            .on('error', onError);
+    });
+    
+    gulp.task('changelog', function() {
+        var version = 'v' + Info.version,
+            name    = 'ChangeLog';
+        
+        Util.exec.parallel([
+            Util.exec.with(exec, 'shell/log.sh ' + version),
+            Util.exec.with(fs.readFile, name, 'utf8'),
+            ], function(error, execParams, fileData) {
+                var DATA        = 0,
+                    STD_ERR     = 1,
+                    execData    = execParams[DATA],
+                    date        = Util.getShortDate(),
+                    head        = date + ', ' + version + '?\n\n',
+                    data        = head + execData + fileData;
+                
+                error   = error || execParams[STD_ERR];
+                
+                if (error)
+                    console.log(error);
+                else
+                    fs.writeFile(name, data, function(error) {
+                        var msg = 'changelog: done';
+                        
+                        console.log(error || msg);
+                    });
+            });
+    });
+    
+    gulp.task('default', ['jshint']);
+    
+    function onError(params) {
+        console.log(params.message);
+    }
+    
+})();
