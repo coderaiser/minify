7889926a83c00d4463646e5034609cbc7f57887f
coderaiser <mnemonic.enemy@gmail.com>
2014-08-19 07:26:36 -0400

    fix(minify) work with streams in node spawn

--- bin/minify.js
+++ bin/minify.js
@@ -7,40 +7,68 @@
     'use strict';
     
     var Util        = require('util-io'),
-        pipe        = require('pipe-io'),
         
         Pack        = require('../package.json'),
         Version     = Pack.version,
         
         log         = function() {
             console.log.apply(console, arguments);
+            process.stdin.pause();
         },
         
         getMinify   = function() {
             return require('..');
         },
         
-        isTTY       = process.stdin.isTTY,
-        
         Argv        = process.argv,
         files       = Util.slice(Argv, 2),
         In          = files[0];
         
-        log.error   = console.error;
+        log.error   = function() {
+            console.error.apply(console, arguments);
+            process.stdin.pause();
+        };
     
     process.on('uncaughtException', function(error) {
         if (error.code !== 'EPIPE')
             log(error.message);
     });
     
-    if (isTTY)
-        minify();
-    else
-        pipe.getBody(process.stdin, function(error, chunks) {
-            minify(chunks);
-        });
+    minify();
     
-    function minify(chunks) {
+    function readStd(callback) {
+        var stdin   = process.stdin,
+            chunks  = '',
+            read    = function() {
+                var chunk = stdin.read();
+                
+                if (chunk) {
+                    chunks += chunk;
+                } else {
+                    stdin.removeListener('readable', read);
+                    callback(chunks);
+                }
+            };
+        
+        stdin.setEncoding('utf8');
+        stdin.addListener('readable', read);
+    }
+    
+    function minify() {
+        if (!In || Util.strCmp(In, ['-h', '--help']))
+            log('minify <input-file1> <input-file2> <inputfileN>\n');
+        
+        else if (Util.strCmp(In, ['-js', '-css', '-html']))
+            readStd(processStream);
+        
+        else if (Util.strCmp(In, ['-v', '--version']) )
+            log('v' + Version);
+        
+        else if (In)
+            uglifyFiles(files);
+    }
+    
+    function processStream(chunks) {
         if (chunks && In)
             getMinify().optimizeData({
                 ext     : Util.replaceStr(In, '-', '.'),
@@ -51,15 +79,6 @@
                 else
                     log(data);
             });
-        
-        else if (!In || Util.strCmp(In, ['-h', '--help']))
-            log('minify <input-file1> <input-file2> <inputfileN>\n');
-        
-        else if (Util.strCmp(In, ['-v', '--version']) )
-            log('v' + Version);
-        
-        else if (In)
-            uglifyFiles(files);
     }
     
     function uglifyFiles(files) {
@@ -73,11 +92,12 @@
         
         Util.exec.parallel(funcs, function(error) {
             var args = Util.slice(arguments, 1);
+            
             if (error)
                 log.error(error.message);
             else
                 log.apply(null, args);
         });
     }
-
+    
 })();
