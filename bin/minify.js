5212ea54279a88023b90487b707958f7f40d3779
coderaiser <coderaiser@cloudcmd.io>
2016-11-09 16:09:43 +0200

    chore(minify) rm iife

--- bin/minify.js
+++ bin/minify.js
@@ -1,111 +1,106 @@
 #!/usr/bin/env node
 
-/* minify binary
- * usage: node minify <input-file> <output-file>
- */
-(function() {
-    'use strict';
+'use strict';
+
+var exec        = require('execon'),
+    Pack        = require('../package.json'),
+    Version     = Pack.version,
     
-    var exec        = require('execon'),
-        Pack        = require('../package.json'),
-        Version     = Pack.version,
-        
-        log         = function() {
-            console.log.apply(console, arguments);
-            process.stdin.pause();
-        },
-        
-        Argv        = process.argv,
-        files       = Argv.slice(2),
-        In          = files[0];
-        
-        log.error   = function() {
-            console.error.apply(console, arguments);
-            process.stdin.pause();
+    log         = function() {
+        console.log.apply(console, arguments);
+        process.stdin.pause();
+    },
+    
+    Argv        = process.argv,
+    files       = Argv.slice(2),
+    In          = files[0];
+    
+    log.error   = function() {
+        console.error.apply(console, arguments);
+        process.stdin.pause();
+    };
+
+process.on('uncaughtException', function(error) {
+    if (error.code !== 'EPIPE')
+        log(error.message);
+});
+
+minify();
+
+function readStd(callback) {
+    var stdin   = process.stdin,
+        chunks  = '',
+        read    = function() {
+            var chunk = stdin.read();
+            
+            if (chunk) {
+                chunks += chunk;
+            } else {
+                stdin.removeListener('readable', read);
+                callback(chunks);
+            }
         };
     
-    process.on('uncaughtException', function(error) {
-        if (error.code !== 'EPIPE')
-            log(error.message);
-    });
+    stdin.setEncoding('utf8');
+    stdin.addListener('readable', read);
+}
+
+function minify() {
+    if (!In || /^(-h|--help)$/.test(In))
+        help();
     
-    minify();
+    else if (/^--(js|css|html)$/.test(In))
+        readStd(processStream);
     
-    function readStd(callback) {
-        var stdin   = process.stdin,
-            chunks  = '',
-            read    = function() {
-                var chunk = stdin.read();
-                
-                if (chunk) {
-                    chunks += chunk;
-                } else {
-                    stdin.removeListener('readable', read);
-                    callback(chunks);
-                }
-            };
-        
-        stdin.setEncoding('utf8');
-        stdin.addListener('readable', read);
-    }
+    else if (/^(-v|--version)$/.test(In))
+        log('v' + Version);
     
-    function minify() {
-        if (!In || /^(-h|--help)$/.test(In))
-            help();
-        
-        else if (/^--(js|css|html)$/.test(In))
-            readStd(processStream);
-        
-        else if (/^(-v|--version)$/.test(In))
-            log('v' + Version);
-        
-        else
-            uglifyFiles(files);
-    }
+    else
+        uglifyFiles(files);
+}
+
+function processStream(chunks) {
+    var name,
+        minify = require('..');
     
-    function processStream(chunks) {
-        var name,
-            minify = require('..');
+    if (chunks && In) {
+        name = In.replace('--', '');
         
-        if (chunks && In) {
-            name = In.replace('--', '');
-            
-            minify[name](chunks, function(error, data) {
-                if (error)
-                    log.error(error.message);
-                else
-                    log(data);
-            });
-        }
-    }
-    
-    function uglifyFiles(files) {
-        var funcs = files.map(function(current) {
-            var minify = require('..');
-            
-            return minify.bind(null, current);
-        });
-        
-        exec.parallel(funcs, function(error) {
-            var args = [].slice.call(arguments, 1);
-            
+        minify[name](chunks, function(error, data) {
             if (error)
                 log.error(error.message);
             else
-                log.apply(null, args);
+                log(data);
         });
     }
-    
-    function help() {
-        var bin         = require('../json/help'),
-            usage       = 'Usage: minify [options]';
+}
+
+function uglifyFiles(files) {
+    var funcs = files.map(function(current) {
+        var minify = require('..');
         
-        console.log(usage);
-        console.log('Options:');
-        
-        Object.keys(bin).forEach(function(name) {
-            console.log('  %s %s', name, bin[name]);
-        });
-    }
+        return minify.bind(null, current);
+    });
     
-})();
+    exec.parallel(funcs, function(error) {
+        var args = [].slice.call(arguments, 1);
+        
+        if (error)
+            log.error(error.message);
+        else
+            log.apply(null, args);
+    });
+}
+
+function help() {
+    var bin         = require('../json/help'),
+        usage       = 'Usage: minify [options]';
+    
+    console.log(usage);
+    console.log('Options:');
+    
+    Object.keys(bin).forEach(function(name) {
+        console.log('  %s %s', name, bin[name]);
+    });
+}
+
