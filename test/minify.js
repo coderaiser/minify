4511513271e2e5060f1225e3cea37aacefbf5c30
James Dawson <james.dawson@publicissapient.com>
2020-01-21 15:57:38 -0600

    feature(minify) add unit tests for passing options to JS, CSS, HTML; correct tests that were double-minifying items; update naming conventions in tests

--- test/minify.js
+++ test/minify.js
@@ -13,10 +13,30 @@ const htmlMinifier = require('html-minifier');
 test('js', async (t) => {
     const js = 'function hello(world) {\nconsole.log(world);\n}';
     
-    const data = await minify.js(js);
-    const min = terser.minify(data).code;
+    const minifyOutput = await minify.js(js);
+    const terserOutput = terser.minify(js).code;
     
-    t.equal(data, min, 'js output should be equal');
+    t.equal(minifyOutput, terserOutput, 'js output should be equal');
+    t.end();
+});
+
+test('js: with alternate options', async (t) => {
+    const js = 'function isTrueFalse() { if (true !== false) { return true; } }';
+    
+    const options = {
+        js: {
+            compress: {
+                booleans_as_integers: true,
+            },
+        },
+    };
+    
+    const minifyOutputWithoutOptions = await minify.js(js);
+    const minifyOutput = await minify.js(js, options);
+    const terserOutput = terser.minify(js, options.js).code;
+    
+    t.equal(minifyOutput, terserOutput, 'js output should be equal');
+    t.notEqual(minifyOutput, minifyOutputWithoutOptions, 'options should influence the output');
     t.end();
 });
 
@@ -46,32 +66,71 @@ test('html', async (t) => {
         minifyCSS:                      true,
     };
     
-    const data = await minify.html(html);
-    const min = htmlMinifier.minify(data, options);
+    const minifyOutput = await minify.html(html);
+    const htmlMinifierOutput = htmlMinifier.minify(html, options);
     
-    t.equal(data, min, 'html output should be equal');
+    t.equal(minifyOutput, htmlMinifierOutput, 'html output should be equal');
+    t.end();
+});
+
+test('html: with alternate options', async (t) => {
+    const html = '<html>\n<body>\nhello world\n</body></html>';
+    
+    const options = {
+        html: {
+            removeOptionalTags: false,
+        },
+    };
+    
+    const minifyOutputWithoutOptions = await minify.html(html);
+    const minifyOutput = await minify.html(html, options);
+    const htmlMinifierOutput = htmlMinifier.minify(minifyOutput, options.html);
+    
+    t.equal(minifyOutput, htmlMinifierOutput, 'html output should be equal');
+    t.notEqual(minifyOutput, minifyOutputWithoutOptions, 'options should influence output');
     t.end();
 });
 
 test('css', async (t) => {
     const css = 'color: #FFFFFF';
     
-    const data = await minify.css(css);
+    const minifyOutput = await minify.css(css);
     const {styles} = new CleanCSS().minify(css);
     
-    t.equal(data, styles, 'css output should be equal');
+    t.equal(minifyOutput, styles, 'css output should be equal');
+    t.end();
+});
+
+test('css: with alternate options', async (t) => {
+    const css = '.gradient { -ms-filter: \'progid:DXImageTransform.Microsoft.Gradient(startColorStr="#ffffff", endColorStr="#000000", GradientType=1)\'; background-image: linear-gradient(to right, #ffffff 0%, #000000 100%); }';
+    const options = {
+        css: {
+            compatibility: {
+                properties: {
+                    ieFilters: true,
+                },
+            },
+        },
+    };
+    
+    const minifyOutputWithoutOptions = await minify.css(css);
+    const minifyOutput = await minify.css(css, options);
+    const {styles} = new CleanCSS(options.css).minify(css);
+    
+    t.equal(minifyOutput, styles, 'css output should be equal');
+    t.notEqual(minifyOutput, minifyOutputWithoutOptions, 'options should influence output');
     t.end();
 });
 
 test('css: base64', async (t) => {
     const dir = `${__dirname}/fixtures`;
-    const name = `${dir}/style.css`;
-    const nameMin = `${dir}/style.min.css`;
+    const pathToCSS = `${dir}/style.css`;
+    const pathToMinifiedCSS = `${dir}/style.min.css`;
     
-    const min = fs.readFileSync(nameMin, 'utf8');
-    const data = await minify(name);
+    const minifiedCSS = fs.readFileSync(pathToMinifiedCSS, 'utf8');
+    const outputCSS = await minify(pathToCSS);
     
-    t.equal(data, min, 'should equal');
+    t.equal(outputCSS, minifiedCSS, 'should be equal');
     t.end();
 });
 
