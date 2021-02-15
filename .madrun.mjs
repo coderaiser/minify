6cd7b52b0fa6d6503a60248766a41b3108a75d7b
coderaiser <coderaiser@cloudcmd.io>
2021-02-15 14:16:19 +0200

    chore(minify) lint

--- .madrun.mjs
+++ .madrun.mjs
@@ -2,8 +2,8 @@ import {run} from 'madrun';
 
 export default {
     'test': () => 'tape test/*.js',
-    'coverage': () => 'nyc npm test',
-    'report': () => 'nyc report --reporter=lcov',
+    'coverage': () => 'c8 npm test',
+    'report': () => 'c8 report --reporter=lcov',
     'fix:lint': () => run('lint', '--fix'),
     'lint': () => 'putout .',
     'fresh:lint': () => run('lint', '--fresh'),
