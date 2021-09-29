91f5c2be4385033d5b59ec4ebe554638e206654b
Michael Rochester <michael.rochester@aexp.com>
2021-09-29 17:25:10 +0100

    linting

--- bin/minify.js
+++ bin/minify.js
@@ -5,7 +5,7 @@
 const {writeFile} = require('fs/promises');
 const tryToCatch = require('try-to-catch');
 const Pack = require('../package');
-const yargsParser = require("yargs-parser");
+const yargsParser = require('yargs-parser');
 const Version = Pack.version;
 
 const log = function(...args) {
@@ -14,7 +14,7 @@ const log = function(...args) {
 };
 
 const Argv = process.argv;
-const args = yargsParser(Argv.slice(2))
+const args = yargsParser(Argv.slice(2));
 const [In] = args._;
 
 log.error = (e) => {
@@ -34,59 +34,59 @@ function readStd(callback) {
     let chunks = '';
     const read = () => {
         const chunk = stdin.read();
-
+        
         if (chunk)
             return chunks += chunk;
-
+        
         stdin.removeListener('readable', read);
         callback(chunks);
     };
-
+    
     stdin.setEncoding('utf8');
     stdin.addListener('readable', read);
 }
 
 async function minify(args) {
-    const {hasArg} = await import("../lib/cli.mjs");
-
+    const {hasArg} = await import('../lib/cli.mjs');
+    
     if (!In || hasArg(['h', 'help'], args))
         return help();
-
+    
     if (hasArg(['js', 'css', 'html'], args))
         return readStd(processStream);
-
+    
     if (hasArg(['v', 'version'], args))
         return log('v' + Version);
-
+    
     const readOptions = await import('../lib/read-options.mjs');
     const [optionsError, options] = await tryToCatch(readOptions);
-
+    
     if (optionsError)
         return log.error(optionsError.message);
-
+    
     uglifyFiles(args._, options, args);
 }
 
 async function processStream(chunks) {
     const minify = require('..');
-
+    
     if (!chunks || !In)
         return;
-
+    
     const name = In.replace('--', '');
-
+    
     const [e, data] = await tryToCatch(minify[name], chunks);
-
+    
     if (e)
         return log.error(e);
-
+    
     log(data);
 }
 
 function uglifyFiles(files, options, flags) {
     const minify = require('..');
     const minifiers = files.map((file) => minify(file, options));
-
+    
     Promise.all(minifiers)
         .then(async (minifiedStrings) => {
             if (flags.overwriteSource)
@@ -111,10 +111,10 @@ function logAll(array) {
 function help() {
     const bin = require('../help');
     const usage = 'Usage: minify [options]';
-
+    
     console.log(usage);
     console.log('Options:');
-
+    
     for (const name of Object.keys(bin)) {
         console.log('  %s %s', name, bin[name]);
     }
