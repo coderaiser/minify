f3ccf716175c272178f12bc9f8db2b0762a17b06
coderiaser <coderaiser@cloudcmd.io>
2026-03-04 18:31:32 +0200

    chore: lint

--- .madrun.mjs
+++ .madrun.mjs
@@ -2,7 +2,7 @@ import {run} from 'madrun';
 
 export default {
     'test': () => `tape 'test/*.js' 'lib/**/*.spec.js'`,
-    'coverage': async () => `c8 "${await run('test')}"`,
+    'coverage': async () => `c8 ${await run('test')}`,
     'fix:lint': () => run('lint', '--fix'),
     'lint': () => 'putout .',
     'fresh:lint': () => run('lint', '--fresh'),
